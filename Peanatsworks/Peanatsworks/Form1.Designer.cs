c941d9608ad90aaeee2b2de1a1e2a670b6a77e6b
Peanatsisnats <161781569+Peanatsisnats@users.noreply.github.com>
2024-05-06 09:58:02 +0800

    Add files via upload

---
+++ Peanatsworks/Peanatsworks/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Peanatsworks
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            checkBox1 = new CheckBox();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DodgerBlue;
+            button1.Font = new Font("Microsoft JhengHei", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(102, 391);
+            button1.Name = "button1";
+            button1.Size = new Size(219, 54);
+            button1.TabIndex = 0;
+            button1.Text = "Sign In";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DodgerBlue;
+            button2.Font = new Font("Microsoft JhengHei", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(102, 502);
+            button2.Name = "button2";
+            button2.Size = new Size(219, 54);
+            button2.TabIndex = 1;
+            button2.Text = "Close";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft PhagsPa", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(163, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 38);
+            label1.TabIndex = 2;
+            label1.Text = "Sign In";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(82, 250);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft JhengHei", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(83, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Username";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(82, 192);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(253, 39);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(82, 281);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(253, 41);
+            textBox2.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Microsoft JhengHei UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(100, 347);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(154, 21);
+            checkBox1.TabIndex = 6;
+            checkBox1.Text = "Remember Password";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft JhengHei", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(102, 466);
+            label4.Name = "label4";
+            label4.Size = new Size(149, 17);
+            label4.TabIndex = 7;
+            label4.Text = "Don't have an account?";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(448, 650);
+            Controls.Add(label4);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private CheckBox checkBox1;
+        private Label label4;
+    }
+}
